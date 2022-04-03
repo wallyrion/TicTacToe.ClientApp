ada1e5d8a2407aa18620bd72dd5a0e5dc93e4ad9
Oleksii Korniienko <alexey.kornienko@smartodds.co.uk>
2022-04-03 22:30:52 +0300

    Implement game field

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+export enum Mark {
+  NA,
+  X,
+  O,
+}
+
+interface GameObject {
+  mark: Mark
+}
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-
+  public mark = Mark;
+  public field: GameObject[] = []!
   constructor() { }
 
   ngOnInit(): void {
+    this.field = this.fillArrayWithCells()
   }
 
+  public onCheckMark(item: GameObject) {
+    item.mark = Mark.X;
+    this.field = [...this.field]
+  }
+
+  private fillArrayWithCells() {
+    var list = [];
+    for (var i = 0; i < 9; i++) {
+      var obj = {
+        mark: Mark.NA
+      }
+      list.push(obj);
+    }
+    return list;
+  }
 }
