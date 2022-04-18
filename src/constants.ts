6e5d5d49a86964a7dbbc4eaf55dbbdc3c3512309
Oleksii Korniienko <alexey.kornienko@smartodds.co.uk>
2022-04-18 13:18:38 +0300

    Test commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,2 @@
-//export const BASE_URL = 'https://tictactoeapi20220404003047.azurewebsites.net'
  export const BASE_URL = 'https://tictactoeapi20220404111722.azurewebsites.net'
 //export const BASE_URL = 'https://localhost:7270'
