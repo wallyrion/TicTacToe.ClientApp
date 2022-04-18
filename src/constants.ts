85195b6e947e54a3baa3d05339a87370a8e210c5
Oleksii Korniienko <alexey.kornienko@smartodds.co.uk>
2022-04-18 13:15:45 +0300

    Create prod build

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 //export const BASE_URL = 'https://tictactoeapi20220404003047.azurewebsites.net'
-// export const BASE_URL = 'https://tictactoeapi20220404111722.azurewebsites.net'
-export const BASE_URL = 'https://localhost:7270'
+ export const BASE_URL = 'https://tictactoeapi20220404111722.azurewebsites.net'
+//export const BASE_URL = 'https://localhost:7270'
